06f21b53a4b39f414684692268ed6a19d0536676
Dipjyoti Saikia <dipjyoti.saikia@gmail.com>
2012-08-18 16:53:51 +0900

    Complete overhaul of build system

--- common-libs/3rd-party/quagga-lib/include/zebra.h
+++ common-libs/3rd-party/quagga-lib/include/zebra.h
@@ -67,16 +67,12 @@ typedef unsigned char   u_int8_t;
 #include <sys/ksym.h>
 #endif /* HAVE_SYS_KSYM_H */
 #include <syslog.h>
-#ifdef TIME_WITH_SYS_TIME
-# include <sys/time.h>
-# include <time.h>
-#else
 # ifdef HAVE_SYS_TIME_H
 #  include <sys/time.h>
+#  include <time.h>
 # else
 #  include <time.h>
 # endif
-#endif /* TIME_WITH_SYS_TIME */
 #include <sys/uio.h>
 #include <sys/utsname.h>
 #ifdef HAVE_RUSAGE
